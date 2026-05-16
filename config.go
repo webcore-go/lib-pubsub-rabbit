c951ec2a4c322abbb1a41950645c49d112652ad1
vinixsatria <vinixsatria@gmail.com>
2026-05-17 03:46:41 +0800

    Perbaiki definisi config agar bisa dimuat melalui mekanisme 'Others'

--- config.go
+++ config.go
@@ -3,41 +3,46 @@ package rabbitmq
 import "github.com/webcore-go/webcore/infra/config"
 
 type RabbitMQConfig struct {
-	config.PubSubConfig
+	// config.PubSubConfig
 
 	// Exchange      string `mapstructure:"exchange"` // Sama dengan Topic
 	// Queue         string `mapstructure:"queue"` // sama dengan Subscription
 
-	Uri           string `mapstructure:"uri"`           // amqp://user:pass@host:port/vhost
-	ExchangeType  string `mapstructure:"exchange_type"` // direct, fanout, topic, headers, none (tanpa exchage = message queue biasa)
-	Durable       bool   `mapstructure:"durable"`
-	AutoDelete    bool   `mapstructure:"auto_delete"`
-	Exclusive     bool   `mapstructure:"exclusive"`
-	PrefetchCount int    `mapstructure:"prefetch_count"`
+	Subscription  string                `mapstructure:"subscription"`
+	Topic         string                `mapstructure:"topic"`
+	Uri           string                `mapstructure:"uri"`           // amqp://user:pass@host:port/vhost
+	ExchangeType  string                `mapstructure:"exchange_type"` // direct, fanout, topic, headers, none (tanpa exchage = message queue biasa)
+	Durable       bool                  `mapstructure:"durable"`
+	AutoDelete    bool                  `mapstructure:"auto_delete"`
+	Exclusive     bool                  `mapstructure:"exclusive"`
+	PrefetchCount int                   `mapstructure:"prefetch_count"`
+	Producer      config.ProducerConfig `mapstructure:"producer"`
 }
 
 func (c *RabbitMQConfig) SetEnvBindings() map[string]string {
 	return map[string]string{
-		"pubsub.topic":          "PUBSUB_TOPIC",
-		"pubsub.subscription":   "PUBSUB_SUBSCRIPTION",
-		"pubsub.uri":            "PUBSUB_URI",
-		"pubsub.exchange_type":  "PUBSUB_EXCHANGE_TYPE",
-		"pubsub.durable":        "PUBSUB_DURABLE",
-		"pubsub.auto_delete":    "PUBSUB_AUTO_DELETE",
-		"pubsub.exclusive":      "PUBSUB_EXCLUSIVE",
-		"pubsub.prefetch_count": "PUBSUB_PREFETCH_COUNT",
+		"rabbitmq.topic":               "RABBITMQ_TOPIC",
+		"rabbitmq.subscription":        "RABBITMQ_SUBSCRIPTION",
+		"rabbitmq.uri":                 "RABBITMQ_URI",
+		"rabbitmq.exchange_type":       "RABBITMQ_EXCHANGE_TYPE",
+		"rabbitmq.durable":             "RABBITMQ_DURABLE",
+		"rabbitmq.auto_delete":         "RABBITMQ_AUTO_DELETE",
+		"rabbitmq.exclusive":           "RABBITMQ_EXCLUSIVE",
+		"rabbitmq.prefetch_count":      "RABBITMQ_PREFETCH_COUNT",
+		"rabbitmq.producer.attributes": "PUBSUB_PRODUCER_ATTRIBUTES",
 	}
 }
 
 func (c *RabbitMQConfig) SetDefaults() map[string]any {
 	return map[string]any{
-		"pubsub.topic":          "",
-		"pubsub.subscription":   "",
-		"pubsub.uri":            "",
-		"pubsub.exchange_type":  "fanout",
-		"pubsub.durable":        true,
-		"pubsub.auto_delete":    false,
-		"pubsub.exclusive":      false,
-		"pubsub.prefetch_count": 0,
+		"rabbitmq.topic":               "",
+		"rabbitmq.subscription":        "",
+		"rabbitmq.uri":                 "",
+		"rabbitmq.exchange_type":       "fanout",
+		"rabbitmq.durable":             true,
+		"rabbitmq.auto_delete":         false,
+		"rabbitmq.exclusive":           false,
+		"rabbitmq.prefetch_count":      0,
+		"rabbitmq.producer.attributes": make(map[string]string),
 	}
 }
